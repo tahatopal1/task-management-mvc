41cba898b5807fdd6da0f272da6d340cd331f11a
Taha Topal <taha.topal.7@gmail.com>
2021-12-08 22:12:35 +0300

    my first commit

---
+++ src/main/java/com/project/mvcside/MvcSideApplication.java
@@ -0,0 +1,13 @@
+package com.project.mvcside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcSideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcSideApplication.class, args);
+    }
+
+}
